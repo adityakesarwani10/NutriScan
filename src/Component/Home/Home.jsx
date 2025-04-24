7cf035cfc55bd9ea840fd21ec283b92e2d38ebff
Aditya Kesarwani <adityakesarwani109@gmail.com>
2025-04-24 13:18:47 +0530

    Finalizing the site

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -48,9 +48,7 @@ const Home = () => {
             <div className="flex justify-center items-center px-3 py-8">
                 <div className="bg-white/10 border border-white/20 shadow-2xl rounded-2xl p-11 pt-16 max-w-3xl w-full text-gray-500 text-xl leading-relaxed transition-all duration-500 hover:shadow-3xl hover:scale-[1.02]">
                     <p>
-                        <span className="italic text-blue-700 font-bold">NutriScan</span> on a thrilling two-day software and hardware development hackathon,
-                        where participants create <span className="font-bold text-blue-700">innovative applications</span> inspired by given themes
-                        within a defined time frame, elevating their projects to new heights. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore a numquam repellendus. Odit asperiores, quas accusamus modi tempora rerum repudiandae molestias excepturi magni voluptatibus maiores quibusdam facere, eum alias saepe aut iste accusantium nisi deserunt nesciunt perferendis iure aliquid non delectus! Atque officia magni eveniet deleniti et ratione tempora molestias.
+                    <span className="italic text-blue-700 font-bold">NutriScan</span> is an <span className="font-bold text-blue-700">innovative and intelligent</span> website designed to revolutionize the way individuals manage their diet and nutrition. At its core, NutriScan empowers users to make informed and healthier food choices by providing detailed, real-time nutritional information with utmost accuracy and convenience. Whether scanning packaged food items for specific nutrient details or exploring a comprehensive database of common foods like fruits and vegetables, NutriScan equips users with the knowledge they need to take control of their health
                     </p>
                 </div>
             </div>
